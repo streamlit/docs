4152c763db2b6a219ef6163485fb584809e16b14
Jack Shedd <jshedd@thisismess.com>
2021-06-20 13:23:12 -0500

    Updates
    Former-commit-id: 0f1ca9f364358e7cd100d4e8bb2aa4162d0beff5

--- next.config.js
+++ next.config.js
@@ -1,7 +1,12 @@
-// const path = require('path')
-
-// module.exports = {
-//     sassOptions: {
-//         includePaths: [path.join(__dirname, 'styles')],
-//     },
-// }
+module.exports = {
+    webpack: (cfg) => {
+        cfg.module.rules.push(
+            {
+                test: /\.md$/,
+                loader: 'frontmatter-markdown-loader',
+                options: { mode: ['react-component'] }
+            }
+        )
+        return cfg;
+    }
+}
