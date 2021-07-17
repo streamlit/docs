225c258041688383c6809ab38615592c5b256da6
Thiago Teixeira <thiago@streamlit.io>
2021-07-17 15:41:45 -0700

    Fix breadcrumb hrefs, fix breadcrumb link targets.
    Former-commit-id: d0232fc6d5d002cf8723601001e56dcaf8f537aa

--- components/utilities/breadCrumbs.js
+++ components/utilities/breadCrumbs.js
@@ -1,23 +1,25 @@
-import React from "react";
+import React from "react"
 
-import Link from "next/link";
+import Link from "next/link"
 
 export default class BreadCrumbs extends React.Component {
     constructor(props) {
-        super(props);
+        super(props)
     }
 
     formatedTitle(title) {
-        return `${title}`.replace(/\-/g, ' '); 
-    };
+        return `${title}`
+            .replace(/\-/g, ' ')
+            .replace(/\bapi\b/, 'API')
+    }
 
     createLink(index, split) {
-        let pathName = split.slice(0, index + 1).join('/')
-        return pathName;
-    };
+        const pathName = split.slice(0, index + 1).join('/')
+        return `/${pathName}`
+    }
 
     createCrumb(crumb, index, slug) {
-        let formatedCrumb;
+        let formatedCrumb
         if (index == slug.length) {
             formatedCrumb = (
                 <Link href={crumb.link}>
@@ -26,12 +28,15 @@ export default class BreadCrumbs extends React.Component {
             )
         } else {
             formatedCrumb = (
-                <Link href={crumb.link}>
-                    <a className="not-link">{crumb.title} <span aria-hidden="true"> / </span></a>
-                </Link>
+                <>
+                    <Link href={crumb.link}>
+                        <a className="not-link">{crumb.title}</a>
+                    </Link>
+                    /
+                </>
             )
         }
-        return formatedCrumb;
+        return formatedCrumb
     }
 
 
@@ -39,27 +44,29 @@ export default class BreadCrumbs extends React.Component {
         const props = this.props
         const breadcrumbs = []
 
-        if ( props.slug === undefined ) { return `` }
+        if ( props.slug === undefined ) { return '' }
 
         const paths = props.slug.join('/')
         const location = `/${paths}`
+        const slugSoFar = []
 
-        for ( const i in props.slug ) 
+        for ( const i in props.slug )
         {
             const item = String( props.slug[i] )
-            
+            slugSoFar.push(item)
+
             if ( i == 0)
             {
                 breadcrumbs.push({
                     link: '/',
-                    title: 'Docs'
+                    title: 'Home'
                 })
             }
-            
+
             if ( i < props.slug.length )
             {
                 breadcrumbs.push({
-                    link: this.createLink(i, props.slug),
+                    link: this.createLink(i, slugSoFar),
                     title: this.formatedTitle(item)
                 })
             }
