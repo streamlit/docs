cfc65a049d55dcee15c9d4c846962b2fa9cc6e8e
Thiago Teixeira <thiago@streamlit.io>
2021-07-18 11:57:24 -0700

    Use not-link instead of custom style in InlineCallout

--- components/blocks/inlineCallout.js
+++ components/blocks/inlineCallout.js
@@ -4,14 +4,14 @@ export default function InlineCallout({ children, icon, color, bold, href }) {
     return (
         <section className="block-inline-callout">
             <Link href={href}>
-                <a className={`icon-box bg-${color}`}>
+                <a className={`icon-box not-link bg-${color}`}>
                     <i>{icon}</i>
                 </a>
             </Link>
             <article>
                 <p>
                     <Link href={href}>
-                        <a className={`color-${color}`}>{bold}</a>
+                        <a className={`not-link color-${color}`}>{bold}</a>
                     </Link>
                     {" "}
                     {children}
