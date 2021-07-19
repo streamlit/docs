32286f4e9a0aff55bf666ec9309361614be0d8dd
Thiago Teixeira <thiago@streamlit.io>
2021-07-18 21:51:16 -0700

    Improve how refCard looks

--- components/blocks/refCard.js
+++ components/blocks/refCard.js
@@ -1,8 +1,12 @@
+import Link from 'next/link'
 
-export default function RefCard({ children, size }) {
+export default function RefCard({ children, size, href }) {
+    // TODO(Thiago): Make code block clickable without taking you to the href.
     return (
-        <section className={`reference-card ${size || 'third'}`}>
-            {children}
-        </section >
+        <Link href={href}>
+            <div className={`not-link reference-card ${size || 'third'}`}>
+                {children}
+            </div>
+        </Link>
     )
 }
