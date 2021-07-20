47f49046d509d8f72c074fa740b4a710312cdfdb
Thiago Teixeira <thiago@streamlit.io>
2021-07-19 21:18:55 -0700

    Make scrollbars less intrusive on Windows and Linux
    Former-commit-id: b64a8fc41d9bf9db0d5ae00197518743f4e35e43

--- pages/_app.js
+++ pages/_app.js
@@ -5,7 +5,7 @@ import '../styles/main.scss'
 import Router from 'next/router'
 import NProgress from 'nprogress'
 
-import { useState, useEffect } from 'react';
+import { useState, useEffect } from 'react'
 
 Router.events.on( 'routeChangeStart', () => NProgress.start() )
 Router.events.on( 'routeChangeComplete', () => NProgress.done() )
@@ -17,36 +17,40 @@ function useWindowSize() {
   const [windowSize, setWindowSize] = useState({
     width: undefined,
     height: undefined,
-  });
+  })
 
   useEffect(() => {
-    // only execute all the code below in client side
-    if (typeof window !== 'undefined') {
-      // Handler to call on window resize
-      function handleResize() {
-        // Set window width/height to state
-        setWindowSize({
-          width: window.innerWidth,
-          height: window.innerHeight,
-        });
-      }
-
-      // Add event listener
-      window.addEventListener("resize", handleResize);
-
-      // Call handler right away so state gets updated with initial window size
-      handleResize();
-
-      // Remove event listener on cleanup
-      return () => window.removeEventListener("resize", handleResize);
+    // Handler to call on window resize
+    function handleResize() {
+      // Set window width/height to state
+      setWindowSize({
+        width: window.innerWidth,
+        height: window.innerHeight,
+      })
     }
-  }, []); // Empty array ensures that effect is only run on mount
-  return windowSize;
+
+    // Add event listener
+    window.addEventListener("resize", handleResize)
+
+    // Call handler right away so state gets updated with initial window size
+    handleResize()
+
+    // Remove event listener on cleanup
+    return () => window.removeEventListener("resize", handleResize)
+  }, []) // Empty array ensures that effect is only run on mount
+
+  useEffect(() => {
+    if (navigator.platform.match('Mac') === null) {
+      document.body.classList.add('mac')
+    }
+  })
+
+  return windowSize
 }
 
 
 function StreamlitDocs({ Component, pageProps }) {
-  const size = useWindowSize();
+  const size = useWindowSize()
   return <Component window={{ width: size.width, height: size.height }} {...pageProps} />
 }
 
