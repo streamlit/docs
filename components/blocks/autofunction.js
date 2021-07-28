7a29d6203a60ba21ffacc6e441aec96afbc86c84
Jack Shedd <jshedd@thisismess.com>
2021-07-27 23:27:08 -0500

    Fixing code block in Autofunc

--- components/blocks/autofunction.js
+++ components/blocks/autofunction.js
@@ -2,12 +2,10 @@ import sortBy from "lodash/sortBy"
 import React from "react"
 import Table from "./table"
 import { H2 } from './headers'
+import { useInView } from 'react-intersection-observer'
 
 import Prism from 'prismjs'
-import "prismjs/components/prism-jsx"
 import 'prismjs/components/prism-python'
-import 'prismjs/components/prism-toml'
-import 'prismjs/components/prism-bash'
 import 'prismjs/plugins/line-numbers/prism-line-numbers'
 import 'prismjs/plugins/line-highlight/prism-line-highlight'
 import 'prismjs/plugins/line-highlight/prism-line-highlight.css'
@@ -16,8 +14,11 @@ import 'prismjs/plugins/copy-to-clipboard/prism-copy-to-clipboard'
 import 'prismjs/plugins/normalize-whitespace/prism-normalize-whitespace'
 
 export default class Autofunction extends React.Component {
+    
     constructor(props) {
         super(props)
+        this.highlighted = false
+        this.blockRef = React.createRef()
     }
 
     Heading(props) {
@@ -25,31 +26,49 @@ export default class Autofunction extends React.Component {
     }
 
     componentDidMount() {
-        if (!window.initial.prism) {
-            window.initial.prism = true;
-            Prism.highlightAll();
-        }
+        this.highlightWithPrism()
     }
 
-    componentWillUnmount() {
-        window.initial.prism = false;
+    highlightWithPrism() {
+        if (this.highlighted) { return; }
+            
+        const pres = Array.prototype.slice.call(this.blockRef.current.getElementsByTagName('pre'))
+
+        pres.forEach((ele) => {
+            const codeText = ele.innerHTML
+            const preTag = ele.cloneNode(true)
+            const codeWrap = document.createElement('div')
+            codeWrap.setAttribute('class', 'block-code')
+            const codeTag = document.createElement('code')
+            codeTag.setAttribute('class', 'language-python')
+            preTag.classList.add('line-numbers')
+            codeTag.innerHTML = codeText
+            preTag.textContent = null
+            preTag.appendChild(codeTag)
+            codeWrap.appendChild(preTag)
+            ele.replaceWith(codeWrap)
+        })
+
+        Prism.highlightAllUnder(this.blockRef.current)
+
+        this.highlighted = true
     }
 
     render() {
+        
         const props = this.props
-        let func_obj
-        let func_description
-        let header
         const footers = []
-
         const rows = []
         const all_versions = Object.keys(props.streamlit)
         const versions = sortBy(all_versions, [ (o) => { return parseFloat(o) }])
         const current_version = versions[versions.length-1]
-
         const components = {
             pre: (props) => <Code {...props} />,
         }
+
+        let func_obj
+        let func_description
+        let header
 
         if (props.function in props.streamlit[current_version]) {
             func_obj = props.streamlit[current_version][props.function]
@@ -103,8 +122,8 @@ export default class Autofunction extends React.Component {
             )
         }
 
-        return (
-            <div className='autofunction'>
+        return (            
+            <div className='autofunction' ref={this.blockRef}>
                 {header}
                 <Table
                     head={{
