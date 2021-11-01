dc674736afb6cef089825e359e50a8bac9f7e61c
Juan Martín García <hello@juangarcia.com.ar>
2021-11-01 13:15:13 -0300

    Replace <Link> component with <a> tags

--- components/navigation/navChild.js
+++ components/navigation/navChild.js
@@ -87,12 +87,10 @@ function NavChild(props) {
 
   link = (
     <span className={`child-item ${active ? "active" : ""}`}>
-      <Link href={url}>
-        <a className="not-link" target={target}>
-          <span className={`colored-ball bg-${props.color}`} />
-          <span>{props.page.name}</span> {icon}
-        </a>
-      </Link>
+      <a className="not-link" target={target} href={url}>
+        <span className={`colored-ball bg-${props.color}`} />
+        <span>{props.page.name}</span> {icon}
+      </a>
       {accordion}
     </span>
   );
