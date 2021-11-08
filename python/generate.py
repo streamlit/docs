af78be067ed24cdc26b44df7e68c0fd736ec7fac
snehankekre <snehan.minerva@gmail.com>
2021-11-08 11:47:38 +0530

    Add long descriptions to API reference docstrings

--- python/generate.py
+++ python/generate.py
@@ -42,7 +42,7 @@ def get_function_docstring_dict(func, signature_prefix):
                 collapsed = '\n'.join(numpydoc_obj['Notes'])
                 description['notes'] = parse_rst(collapsed)
 
-            if 'Warning' in numpydoc_obj and len(numpydoc_obj['Warning']) > 0:
+            if 'Warning' in (numpydoc_obj) and len(numpydoc_obj['Warning']) > 0:
                 collapsed = '\n'.join(numpydoc_obj['Warning'])
                 description['warnings'] = parse_rst(collapsed)
 
@@ -57,8 +57,14 @@ def get_function_docstring_dict(func, signature_prefix):
             pass
 
         docstring_obj = docstring_parser.parse(docstring)
+        short_description = docstring_obj.short_description
+        long_description = str('' if docstring_obj.long_description is None else docstring_obj.long_description)
 
-        description['description'] = docstring_obj.short_description
+        # Insert a blank line between the short and long description, if the latter exists.
+        if long_description:
+            description['description'] = parse_rst('\n\n'.join([short_description, long_description]))
+        else:
+            description['description'] = short_description
 
         description['args'] = []
         for param in docstring_obj.params:
