60c32958191b56b381e8e58d2d93dc6a449c9f06
Juan Martín García <hello@juangarcia.com.ar>
2021-11-17 08:56:04 -0300

    Refactor Header component
    Former-commit-id: 78f7b9c88baeb511f88640a0c071a7ab64e1faca

--- components/navigation/header.js
+++ components/navigation/header.js
@@ -1,72 +1,61 @@
-// import Navigation from
-import React from "react";
-
+import React, { useState, useEffect } from "react";
 import Link from "next/link";
 import dynamic from "next/dynamic";
 
+// import Navigation from
 import MobileNav from "./mobileNav";
 
 const ThemeToggle = dynamic(() => import("../utilities/themeToggle"), {
   ssr: false,
 });
-
 import Search from "../utilities/search";
 
-export default class Header extends React.Component {
-  constructor(props) {
-    super(props);
-    this.handleScroll = this.handleScroll.bind(this);
-    this.handleResize = this.handleResize.bind(this);
-    this.state = {
-      sticky: false,
-    };
-  }
+const Header = () => {
+  const [isSticky, setIsSticky] = useState(false);
+  const [windowWidth, setWindowWidth] = useState();
 
-  componentDidMount() {
-    window.addEventListener("scroll", this.handleScroll);
-    window.addEventListener("resize", this.handleResize);
-    this.handleResize();
-  }
+  const handleResize = () => {
+    setWindowWidth({ windowWidth: window.innerWidth });
+  };
 
-  handleResize() {
-    this.setState({ windowWidth: window.innerWidth });
-  }
-
-  componentWillUnmount() {
-    window.removeEventListener("scroll", this.handleScroll);
-    window.removeEventListener("resize", this.handleResize);
-  }
-
-  handleScroll() {
+  const handleScroll = () => {
     let top = window.scrollY;
-    top > 20
-      ? this.setState({ sticky: true })
-      : this.setState({ sticky: false });
+    top > 20 ? setIsSticky(true) : setIsSticky(false);
+  };
+
+  useEffect(() => {
+    handleResize();
+    window.addEventListener("scroll", handleScroll);
+    window.addEventListener("resize", handleResize);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
+  let mobileNav;
+  if (windowWidth <= 1024) {
+    mobileNav = <MobileNav />;
   }
 
-  render() {
-    let mobileNav;
+  return (
+    <header className={`${isSticky ? "sticky" : ""}`}>
+      <nav className="container" id="main-header">
+        <Link href="/">
+          <a className="brand not-link">
+            <img src="/logo.svg" alt="" />
+            <h4>Documentation</h4>
+          </a>
+        </Link>
+        <section className="options">
+          <Search />
+          <ThemeToggle />
+          {mobileNav}
+        </section>
+      </nav>
+    </header>
+  );
+};
 
-    if (this.state.windowWidth <= 1024) {
-      mobileNav = <MobileNav />;
-    }
-
-    return (
-      <header className={`${this.state.sticky ? "sticky" : ""}`}>
-        <nav className="container" id="main-header">
-          <Link href="/">
-            <a className="brand not-link">
-              <img src="/logo.svg" alt="" />
-              <h4>Documentation</h4>
-            </a>
-          </Link>
-          <section className="options">
-            <Search />
-            <ThemeToggle />
-            {mobileNav}
-          </section>
-        </nav>
-      </header>
-    );
-  }
-}
+export default Header;
