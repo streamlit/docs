c1665da49a36d767c5737833a281e95f6146aabe
Juan Martín García <hello@juangarcia.com.ar>
2021-11-23 08:45:57 -0300

    Refactor sidebar components
    Former-commit-id: 65c3e07e96732e1c5a81b9517f9e22462b57fd99

--- components/navigation/navChild.js
+++ components/navigation/navChild.js
@@ -1,22 +1,21 @@
-import findIndex from "lodash/findIndex";
 import React, { useState } from "react";
 import Link from "next/link";
 
 import useVersion from "../../lib/useVersion.js";
 
-function NavChild(props) {
+const NavChild = ({ slug, page, color }) => {
   const [manualState, setManualState] = useState(null);
   const version = useVersion();
 
-  const isNum = /^[\d\.]+$/.test(props.slug[0]);
+  const isNum = /^[\d\.]+$/.test(slug[0]);
 
   if (isNum) {
-    props.slug.shift();
+    slug.shift();
   }
 
-  const slugStr = `/${props.slug.join("/")}`;
-  const active = slugStr === props.page.url ? true : false;
-  const shouldAutoOpen = slugStr.startsWith(props.page.url);
+  const slugStr = `/${slug.join("/")}`;
+  const active = slugStr === page.url ? true : false;
+  const shouldAutoOpen = slugStr.startsWith(page.url);
   const opened = manualState ?? shouldAutoOpen;
 
   let subNav;
@@ -43,7 +42,7 @@ function NavChild(props) {
 
   let accordion;
 
-  if (props.page.children?.length > 0) {
+  if (page.children?.length > 0) {
     accordion = (
       <i
         className={`accordion ${opened ? "close" : "open"}`}
@@ -57,22 +56,18 @@ function NavChild(props) {
   let link;
   let icon;
   let target;
-  let url = props.page.url;
+  let url = page.url;
 
-  const isLocalPage = props.page.url.startsWith("/");
+  const isLocalPage = page.url.startsWith("/");
 
   if (!isLocalPage) {
     icon = <i className="external">open_in_new</i>;
     target = "_blank";
   }
 
-  if (
-    props.page.isVersioned &&
-    version &&
-    isLocalPage
-  ) {
+  if (page.isVersioned && version && isLocalPage) {
     // We need to version this URL, check if the URL has a version for this version
-    const newSlug = props.page.url.split("/");
+    const newSlug = page.url.split("/");
     newSlug[0] = version;
     url = `/${newSlug.join("/")}`;
   }
@@ -81,8 +76,8 @@ function NavChild(props) {
     <span className={`child-item ${active ? "active" : ""}`}>
       <Link href={url}>
         <a className="not-link" target={target}>
-          <span className={`colored-ball bg-${props.color}`} />
-          <span>{props.page.name}</span> {icon}
+          <span className={`colored-ball bg-${color}`} />
+          <span>{page.name}</span> {icon}
         </a>
       </Link>
       {accordion}
