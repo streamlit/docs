31384128eaa7293933915f992e97f8cee57cef6c
Juan Martín García <hello@juangarcia.com.ar>
2021-12-20 15:53:03 -0300

    Adds support to use Tailwind dark option with a class on the HTML

--- pages/_document.js
+++ pages/_document.js
@@ -5,12 +5,16 @@ import Document, { Html, Head, Main, NextScript } from "next/document";
 export default function StreamlitDocument() {
   const setInitialTheme = `
         function getUserPreference() {
-            if(window.localStorage.getItem('theme')) {
+          if(window.localStorage.getItem('theme')) {
             return window.localStorage.getItem('theme')
-            }
-            return window.matchMedia('(prefers-color-scheme: dark)').matches
-            ? 'dark-mode'
-            : 'light-mode'
+          }
+          return window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark-mode' : 'light-mode';
+        }
+
+        if(getUserPreference() === 'dark-mode') {
+          document.documentElement.classList.add('dark');
+        } else {
+          document.documentElement.classList.add('light');
         }
         document.body.dataset.theme = getUserPreference();
         window.initial = { prism: false };
