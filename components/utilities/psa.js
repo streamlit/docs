09c57ffdfcd03fe651720c3f1bcf628e77ef9071
Juan Martín García <hello@juangarcia.com.ar>
2022-01-10 15:42:49 -0300

    Refactor PSA component
    Former-commit-id: 30d2722b2a3fa80ff65135c4f5694433e6366dfc

--- components/utilities/psa.js
+++ components/utilities/psa.js
@@ -1,16 +1,49 @@
 import Link from "next/link";
 
+import PsaStyle from "./psa.module.css";
+
 const Psa = () => {
   return (
-    <section className="block-forum-psa">
-      <i>forum</i>
-      <article className="content">
-        <h3>Still have questions?</h3>
-        <p>
+    <section
+      className={`
+        flex items-center flex-wrap
+        py-8 mt-16
+        border-t border-b border-t-gray-30 border-b-gray-30 dark:border-b-gray-90 dark:border-t-gray-90
+        ${PsaStyle.Container}
+      `}
+    >
+      <i
+        className="
+          text-5xl
+          mr-8
+          dark:text-white
+        "
+      >
+        forum
+      </i>
+      <article>
+        <h3
+          className="
+            my-0
+            font-bold text-2xl
+            dark:text-white
+          "
+        >
+          Still have questions?
+        </h3>
+        <p className="text-base tracking-tight dark:text-white">
           Our{" "}
-          <Link href="https://discuss.streamlit.io" target="_blank">
-            <a>forums</a>
-          </Link>{" "}
+          <a
+            href="https://discuss.streamlit.io"
+            target="_blank"
+            className="
+              border-b border-b-gray-90 dark:border-b-white
+              hover:opacity-70 border-b-red-70
+              dark:text-white
+            "
+          >
+            forums
+          </a>{" "}
           are full of helpful information and Streamlit experts.
         </p>
       </article>
