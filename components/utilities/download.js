8b1342ea9f93cc06e4b7f017cf7f58eaaa78ff66
Juan Martín García <hello@juangarcia.com.ar>
2022-01-19 08:15:55 -0300

    Move tailwind classes to css modules
    Former-commit-id: 370994df024dfcf04199b5f47ae9a38fc71264dc

--- components/utilities/download.js
+++ components/utilities/download.js
@@ -1,17 +1,9 @@
+import styles from "./download.module.css";
+
 // Simple horizontal flex container used for MDX.
 const Download = ({ href, children }) => {
   return (
-    <a
-      href={href}
-      className="
-        font-sans text-base tracking-tight
-        border-b border-b-gray-90 dark:border-b-white dark:hover:border-b-red-70 hover:border-b-red-70
-        text-gray-90 dark:text-white
-        hover:opacity-70
-      "
-      rel="nofollow noopener"
-      download
-    >
+    <a href={href} className={styles.Link} rel="nofollow noopener" download>
       {children}
     </a>
   );
