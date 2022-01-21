5c6b0a0b3ed585f207d02bd6a24831b859903fd2
Juan Martín García <hello@juangarcia.com.ar>
2022-01-21 10:56:49 -0300

    Refactor tileContainer component
    Former-commit-id: 4dbcc80b689b51635d8019985614f332b8f85547

--- components/blocks/refCard.js
+++ components/blocks/refCard.js
@@ -1,9 +1,29 @@
 import Link from "next/link";
 
+import styles from "./refCard.module.css";
+
 const RefCard = ({ children, size, href }) => {
+  const tileSize =
+    size === "full"
+      ? styles.Full
+      : size === "half"
+      ? styles.Half
+      : size === "third"
+      ? styles.Third
+      : size === "two-third"
+      ? styles.TwoThirds
+      : styles.Third;
   return (
     <Link href={href}>
-      <a className={`not-link reference-card ${size || "third"}`}>{children}</a>
+      <a
+        className={`
+          not-link 
+          ${styles.Container}
+          ${tileSize}
+        `}
+      >
+        {children}
+      </a>
     </Link>
   );
 };
