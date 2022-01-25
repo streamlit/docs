3d022adc6c8796d893b162ccb429a34c10c9a20c
Snehan Kekre <snehan.minerva@gmail.com>
2022-01-25 13:02:38 +0530

    Fix image path

--- python/api-examples-source/widget.download_button.py
+++ python/api-examples-source/widget.download_button.py
@@ -32,7 +32,7 @@ binary_contents = b'example content'
 # Defaults to 'application/octet-stream'
 st.download_button('Download binary file', binary_contents)
 
-with open("flower.png", "rb") as file:
+with open("python/api-examples-source/flower.png", "rb") as file:
     btn = st.download_button(
         label="Download image",
         data=file,
