951b8933b8dc97ab44f00126d151609ac149d2e3
Juan Martín García <hello@juangarcia.com.ar>
2022-01-28 10:18:35 -0300

    Refactor Note and Warning components
    Former-commit-id: c4f488807f4927459ff56a586eda5e4c68f29553

--- components/blocks/noted.js
+++ components/blocks/noted.js
@@ -1,53 +1,24 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
+import classNames from "classnames";
+
+import CalloutStyles from "./callout.module.css";
+import NoteStyles from "./note.module.css";
 
 import IconHeader from "./iconHeader";
 
 const Note = ({ children }) => {
-  const [theme, setTheme] = useState("light-mode");
-
-  useEffect(() => {
-    window.addEventListener("ChangeTheme", handleTheme);
-
-    return () => {
-      window.removeEventListener("ChangeTheme", handleTheme);
-    };
-  }, []);
-
-  const handleTheme = () => {
-    setTheme(document.body.dataset.theme);
-  };
-
-  let block;
-
-  if (theme == "dark-mode") {
-    block = (
-      <section className={`block-note bg-dark-black color-white`}>
-        <IconHeader
-          icon="push_pin"
-          rotate="45"
-          title="Note"
-          background="l-blue-70"
-          color="white"
-        />
-        {children}
-      </section>
-    );
-  } else {
-    block = (
-      <section className={`block-note bg-l-blue-10 color-gray-90`}>
-        <IconHeader
-          icon="push_pin"
-          rotate="45"
-          title="Note"
-          background="l-blue-70"
-          color="white"
-        />
-        {children}
-      </section>
-    );
-  }
-
-  return block;
+  return (
+    <section className={classNames(CalloutStyles.Container, NoteStyles.Note)}>
+      <IconHeader
+        icon="push_pin"
+        rotate="45"
+        title="Note"
+        background="l-blue-70"
+        color="white"
+      />
+      {children}
+    </section>
+  );
 };
 
 export default Note;
