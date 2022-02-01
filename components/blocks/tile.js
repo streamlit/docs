caa881e0c8cbf8d902f47bc68a106ba4d4744d95
Juan Martín García <hello@juangarcia.com.ar>
2022-02-01 11:09:16 -0300

    Tile CSS Refactor

--- components/blocks/tile.js
+++ components/blocks/tile.js
@@ -1,5 +1,8 @@
 import React, { useState, useEffect } from "react";
+import classNames from "classnames";
 import Link from "next/link";
+
+import styles from "./tile.module.css";
 
 const Tile = ({
   img,
@@ -16,6 +19,17 @@ const Tile = ({
 }) => {
   const [theme, setTheme] = useState("light-mode");
 
+  const tileSize =
+    size === "full"
+      ? styles.Full
+      : size === "half"
+      ? styles.Half
+      : size === "third"
+      ? styles.Third
+      : size === "two-third"
+      ? styles.TwoThirds
+      : styles.Third;
+
   useEffect(() => {
     window.addEventListener("ChangeTheme", handleTheme);
 
@@ -31,78 +45,30 @@ const Tile = ({
   let image;
   if (img) {
     image = <img src={img} />;
-  } else if (dark) {
-    image = (
-      <i
-        className={`material-icons-sharp bg-${
-          dark.background || "l-blue-70"
-        } color-${dark.color || "white"}`}
-        style={{ transform: `rotate(${rotate || 0}deg)` }}
-      >
-        {icon || "downloading"}
-      </i>
-    );
   } else {
     image = (
-      <i
-        className={`material-icons-sharp bg-${
-          background || "l-blue-70"
-        } color-${color || "white"}`}
-        style={{ transform: `rotate(${rotate || 0}deg)` }}
-      >
+      <i className={classNames("material-icons-sharp", styles.Icon)}>
         {icon || "downloading"}
       </i>
     );
   }
 
-  let block;
-  if (dark && theme == "dark-mode") {
-    block = (
-      <article
-        className={`block-tile ${size || "third"} bg-${
-          dark.background || "l-blue-70"
-        } color-${dark.color || "white"} border-${
-          dark.borderColor || "transparent"
-        }`}
-      >
-        <Link href={link || "/"}>
-          <a className="not-link">
-            {image}
-            <section className="content">
-              <h4 className="title">{title || "Install Streamlit"}</h4>
-              <p className="small">
-                {text ||
-                  "Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia."}
-              </p>
-            </section>
-          </a>
-        </Link>
-      </article>
-    );
-  } else {
-    block = (
-      <article
-        className={`block-tile ${size || "third"} bg-${
-          background || "l-blue-70"
-        } color-${color || "white"} border-${borderColor || "transparent"}`}
-      >
-        <Link href={link || "/"}>
-          <a className="not-link">
-            {image}
-            <section className="content">
-              <h4 className="title">{title || "Install Streamlit"}</h4>
-              <p className="small">
-                {text ||
-                  "Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia."}
-              </p>
-            </section>
-          </a>
-        </Link>
-      </article>
-    );
-  }
-
-  return block;
+  return (
+    <div className={classNames(styles.Container, tileSize || "third")}>
+      <Link href={link || "/"}>
+        <a className={classNames("not-link", styles.Link)}>
+          {image}
+          <div>
+            <h4 className={styles.Title}>{title || "Install Streamlit"}</h4>
+            <p className={styles.Text}>
+              {text ||
+                "Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia."}
+            </p>
+          </div>
+        </a>
+      </Link>
+    </div>
+  );
 };
 
 export default Tile;
