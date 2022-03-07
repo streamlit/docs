3ab3a8bde0613d6418b1b19533711518fb674037
Juan Martín García <hello@juangarcia.com.ar>
2022-03-07 09:54:00 -0300

    Install loaders for SVG importing as React components
    Former-commit-id: af2b2908c66cf4be5d993707da7c0d431d8322a5

--- next.config.js
+++ next.config.js
@@ -2,10 +2,16 @@ const fs = require("fs");
 
 module.exports = {
   webpack: (configuration) => {
-    configuration.module.rules.push({
-      test: /\.md$/,
-      use: "frontmatter-markdown-loader",
-    });
+    configuration.module.rules.push(
+      {
+        test: /\.md$/,
+        use: "frontmatter-markdown-loader",
+      },
+      {
+        test: /\.svg$/,
+        use: ["@svgr/webpack", "file-loader"],
+      }
+    );
     return configuration;
   },
   async exportPathMap(defaultPathMap) {
