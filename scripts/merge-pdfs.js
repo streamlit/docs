90170d4884e1f7234472eedfdcf80ee717e92ea0
Juan Garcia <jugarcia@c7rqqmwxhr.home>
2022-05-26 16:53:25 -0300

    Initial implementation for offline docs generation

---
+++ scripts/merge-pdfs.js
@@ -0,0 +1,18 @@
+const path = require("path");
+const fs = require("fs");
+const PDFMerger = require("pdf-merger-js");
+
+const merger = new PDFMerger();
+const directoryPath = path.join(__dirname, "../content/pdf/");
+
+const getPDFs = () => {
+  fs.readdir(directoryPath, async function (err, files) {
+    files.forEach(function (file) {
+      merger.add(`${directoryPath}/${file}`);
+    });
+
+    await merger.save(`${directoryPath}/docs-merged.pdf`);
+  });
+};
+
+getPDFs();
