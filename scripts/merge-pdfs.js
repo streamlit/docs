cd8b0e435a07157ee4ab563d213a67505cfcf001
Juan Garcia <jugarcia@c7rqqmwxhr.home>
2022-05-27 11:47:50 -0300

    Improve node scripts
    Former-commit-id: 0ff48ee880a3debcc456075ce646a1b5c10c127a

--- scripts/merge-pdfs.js
+++ scripts/merge-pdfs.js
@@ -1,17 +1,33 @@
 const path = require("path");
 const fs = require("fs");
 const PDFMerger = require("pdf-merger-js");
+const isPDFValid = require("is-pdf-valid");
 
+// Initialize the merger
 const merger = new PDFMerger();
-const directoryPath = path.join(__dirname, "../public/pdf/");
+const pagesPath = path.join(__dirname, "../public/pdf/pages");
+const pdfPath = path.join(__dirname, "../public/pdf");
 
 const getPDFs = () => {
-  fs.readdir(directoryPath, async function (err, files) {
+  console.log("Running the PDF merger...");
+
+  // Get all pdfs inside the directory path
+  fs.readdir(pagesPath, async function (err, files) {
+    // Loop through all files and check its validity
     files.forEach(function (file) {
-      merger.add(`${directoryPath}/${file}`);
+      console.log(`Adding ${file}...`);
+
+      const isFileValid = isPDFValid(fs.readFileSync(`${pagesPath}/${file}`));
+      isFileValid === true ? merger.add(`${pagesPath}/${file}`) : "";
+
+      console.log(`Done! ${file} added`);
     });
 
-    await merger.save(`${directoryPath}/docs-merged.pdf`);
+    console.log("Merging the PDF...");
+
+    await merger.save(`${pdfPath}/offline-docs.pdf`);
+
+    console.log(`Done! PDF generated on ${pdfPath}/offline-docs.pdf`);
   });
 };
 
