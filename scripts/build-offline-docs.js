94e693a7ebd120f0e485c747b12256402b0544cd
Juan Martín García <hello@juangarcia.com.ar>
2022-06-10 17:46:46 -0300

    Update scripts for better sorting

--- scripts/build-offline-docs.js
+++ scripts/build-offline-docs.js
@@ -1,83 +1,31 @@
 const GetSitemapLinks = require("get-sitemap-links").default;
+const matter = require("gray-matter");
 const puppeteer = require("puppeteer");
+const path = require("path");
+const fs = require("fs");
 
-let urls = [];
 let browser;
 
-const getLinks = async () => {
+const getSitemapLinks = async () => {
   // Launch puppeteer
   browser = await puppeteer.launch();
 
-  console.log("Getting the URLs from the sitemap...");
+  console.log("Getting the URLs from the menu...");
 
-  // Get all the docs links from the sitemap
-  const urlsArray = await GetSitemapLinks(
-    "https://docs.streamlit.io/sitemap.xml"
-  );
-
-  console.log(
-    `Done! Finished getting the URLs from the sitemap. Got ${urlsArray.length} URLs`
-  );
-
-  // Array of links that are wrong/throw a 404 error
-  const urlsToRemove = [
-    ".keep",
-    "billing",
-    "code",
-    "community",
-    "deploy-an-app",
-    "errors",
-    "gdpr-banner",
-    "menu",
-    "rent-prices",
-    "security-model",
-    "streamlit-active-directory-adfs",
-    "streamlit-azure-active-directory",
-    "streamlit-general-saml-authentication",
-    "style-guide",
-    "enterprise",
-    "single-sign-on-sso",
-    "streamlit-okta-sso",
-  ];
+  const menuFile = path.join(__dirname, "../content/menu.md");
+  const fileContents = fs.readFileSync(menuFile, "utf8");
+  const matterResult = matter(fileContents);
+  const urls = matterResult.data.site_menu;
 
   console.log(
-    "Starting to remove versioned and pages that we dont want to create PDFs for..."
+    `Finished getting the URLs from the menu. Got ${urls.length} entries`
   );
 
-  // Store urls in array with a bit more metadata
-  urlsArray.map((url) => {
-    // Format slugs like this:
-    // 00-index
-    // 01-library-api-reference-text-st.header
-    // 02-knowledge-base-using-streamlit-remove-streamlit-app-title
-    // 03-streamlit-cloud
-    // So they are a bit more organized when the PDF gets exported
-    let formattedSlug = url
-      .replace("https://docs.streamlit.io/", "")
-      .replaceAll("/", "-")
-      .replaceAll("index", "00-index")
-      .replaceAll("library", "01-library")
-      .replaceAll("knowledge-base", "02-knowledge-base")
-      .replaceAll("streamlit-cloud", "03-streamlit-cloud");
-    urls.push({
-      url: url.replace("https://docs.streamlit.io/", "http://localhost:3000/"),
-      slug: formattedSlug,
-      isVersioned: /^[\d\.]+$/.test(url.split("/")[3]),
-      shouldBeDeleted:
-        urlsToRemove.filter((item) => url.split("/").pop() === item).length > 0
-          ? true
-          : false,
-    });
-  });
+  console.log("Moving on to creating PDFs for these pages...");
 
-  // After we have the data, we remove the versioned and incorrect urls and grab a pdf for the rest
-  const filteredPages = urls.filter(
-    (url) => url.isVersioned === false && url.shouldBeDeleted === false
-  );
+  urls.map((url, index) => (url.order = index));
 
-  console.log("Done! Moving on to creating PDFs for these pages...");
-
-  const promises = filteredPages.map((page) => getPDFs(page));
+  const promises = urls.map((page) => getPDFs(page));
 
   Promise.all(promises).then((results) => {
     console.log("Done creating all PDFs. Exiting the process!");
@@ -87,24 +35,31 @@ const getLinks = async () => {
 
 // Function to go to the page, download the pdf, and close it
 const getPDFs = async (url) => {
-  console.log(`Creating PDF for ${url.slug}...`);
+  console.log(`Creating PDF for ${url.url}...`);
+
+  // If it's an external URL, skip it
+  if (url.url.includes("http")) {
+    return;
+  }
 
   // Puppeteer stuff
   const page = await browser.newPage();
   await page.setDefaultNavigationTimeout(0);
-  await page.goto(`${url.url}`, {
+  await page.goto(`http://localhost:3000${url.url}`, {
     waitUntil: ["networkidle2"],
   });
   await page.emulateMediaType("print");
 
+  const pageName = `${url.order}${url.url.replaceAll("/", "-")}`;
+
   // Creating the PDF
   await page.pdf({
-    path: `public/pdf/pages/${url.slug}.pdf`,
+    path: `public/pdf/pages/${pageName}.pdf`,
     format: "a4",
   });
 
-  console.log(`Done! Created PDF for ${url.slug}`);
+  console.log(`Done! Created PDF for ${url.url}`);
   page.close();
 };
 
-getLinks();
+getSitemapLinks();
