f05f2a634f53a985578a0b41fe5f04e62101f0dd
snehankekre <snehan.minerva@gmail.com>
2022-07-19 19:02:35 +0530

    Add st.pydeck_chart docs app
    Former-commit-id: 925f7ffb40e85b626fee13a0cc443f9319bcbe8c

---
+++ python/api-examples-source/charts.pydeck_chart.py
@@ -0,0 +1,45 @@
+import streamlit as st
+import pydeck as pdk
+import pandas as pd
+import numpy as np
+
+
+@st.experimental_memo
+def load_data():
+    return pd.DataFrame(
+        np.random.randn(1000, 2) / [50, 50] + [37.76, -122.4], columns=["lat", "lon"]
+    )
+
+
+df = load_data()
+
+st.pydeck_chart(
+    pdk.Deck(
+        map_style="mapbox://styles/mapbox/light-v9",
+        initial_view_state=pdk.ViewState(
+            latitude=37.76,
+            longitude=-122.4,
+            zoom=11,
+            pitch=50,
+        ),
+        layers=[
+            pdk.Layer(
+                "HexagonLayer",
+                data=df,
+                get_position="[lon, lat]",
+                radius=200,
+                elevation_scale=4,
+                elevation_range=[0, 1000],
+                pickable=True,
+                extruded=True,
+            ),
+            pdk.Layer(
+                "ScatterplotLayer",
+                data=df,
+                get_position="[lon, lat]",
+                get_color="[200, 30, 0, 160]",
+                get_radius=200,
+            ),
+        ],
+    )
+)
