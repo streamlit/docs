22849accc3b10af9cbb4d1cd1bc9a709cea6875d
snehankekre <snehan.minerva@gmail.com>
2022-09-06 15:26:23 +0530

    Add optional target attribute to Button component
    Former-commit-id: 541282d2ba602492bbce16099b1a8f554ebdb8dc

--- components/blocks/button.js
+++ components/blocks/button.js
@@ -2,12 +2,20 @@ import Link from "next/link";
 
 import styles from "./button.module.css";
 
-const Button = ({ children, link }) => {
-  return (
-    <Link href={link}>
-      <button className={styles.Button}>{children}</button>
-    </Link>
-  );
+const Button = ({ children, link, target }) => {
+  if (target) {
+    return (
+      <a href={link} target={target}>
+        <button className={styles.Button}>{children}</button>
+      </a>
+    );
+  } else {
+    return (
+      <Link href={link}>
+        <button className={styles.Button}>{children}</button>
+      </Link>
+    );
+  }
 };
 
 export default Button;
