3107c80100725e131bf57c82fb3f0896bce9f861
snehankekre <snehan.minerva@gmail.com>
2022-09-07 15:40:57 +0530

    Use ternary operator inplace of if/else
    Former-commit-id: 0d1f981be0cf3100d82355fae12e8a1c713854ce

--- components/blocks/button.js
+++ components/blocks/button.js
@@ -3,24 +3,20 @@ import Link from "next/link";
 import styles from "./button.module.css";
 
 const Button = ({ children, link, target }) => {
-  if (target == "_blank") {
-    return (
-      <a
-        href={link}
-        target={target}
-        rel="noopener noreferrer"
-        className={styles.Button}
-      >
-        {children}
-      </a>
-    );
-  } else {
-    return (
-      <Link href={link}>
-        <a className={styles.Button}>{children}</a>
-      </Link>
-    );
-  }
+  return target == "_blank" ? (
+    <a
+      href={link}
+      target={target}
+      rel="noopener noreferrer"
+      className={styles.Button}
+    >
+      {children}
+    </a>
+  ) : (
+    <Link href={link}>
+      <a className={styles.Button}>{children}</a>
+    </Link>
+  );
 };
 
 export default Button;
