ce80f8bdaf7cf6f5802b875e8e59f7b9409569c3
snehankekre <snehan.minerva@gmail.com>
2022-09-07 15:23:48 +0530

    Explicitly check if target is _blank in button.js

--- components/blocks/button.js
+++ components/blocks/button.js
@@ -3,7 +3,7 @@ import Link from "next/link";
 import styles from "./button.module.css";
 
 const Button = ({ children, link, target }) => {
-  if (target) {
+  if (target == "_blank") {
     return (
       <a href={link} target={target}>
         <button className={styles.Button}>{children}</button>
