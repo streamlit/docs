2ed3f41582ebc4da7012810b263647d774697e0d
snehankekre <snehan.minerva@gmail.com>
2022-09-07 15:32:59 +0530

    Use <a> tags instead of <button> for links

--- components/blocks/button.js
+++ components/blocks/button.js
@@ -5,14 +5,19 @@ import styles from "./button.module.css";
 const Button = ({ children, link, target }) => {
   if (target == "_blank") {
     return (
-      <a href={link} target={target} rel="noopener noreferrer">
-        <button className={styles.Button}>{children}</button>
+      <a
+        href={link}
+        target={target}
+        rel="noopener noreferrer"
+        className={styles.Button}
+      >
+        {children}
       </a>
     );
   } else {
     return (
       <Link href={link}>
-        <button className={styles.Button}>{children}</button>
+        <a className={styles.Button}>{children}</a>
       </Link>
     );
   }
