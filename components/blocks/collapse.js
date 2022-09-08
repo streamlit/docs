a5afdecb86917d3a482867380a0e3dcfdc70c9d3
Juan Martin Garcia <juan.garcia@snowflake.com>
2022-09-08 11:51:05 -0300

    Style updates for collapse panel

--- components/blocks/collapse.js
+++ components/blocks/collapse.js
@@ -6,32 +6,21 @@ const Collapse = ({ title, children }) => {
   const [show, setShow] = useState(false);
 
   return (
-    <section className="py-2">
-      <div className="collapse_container border rounded">
-        <div className="py-2 cursor-pointer" onClick={() => setShow(!show)}>
-          <summary className="flex text-[18px] pl-2 font-abold hover:opacity-60 py-2">
-            {show ? (
-              <i className="rotate-90 duration-100 text-xxl fill-gray-90 self-center pr-[0.2rem]">
-                chevron_right
-              </i>
-            ) : (
-              <i className="fill-gray-90 self-center">chevron_right</i>
+    <section className={styles.Container}>
+      <div className={styles.PanelHeading} onClick={() => setShow(!show)}>
+        <summary className={styles.PanelTitle}>
+          <span className={styles.Title}>{title}</span>
+
+          <i
+            className={classNames(
+              styles.PanelIcon,
+              show ? styles.RotatedIcon : ""
             )}
-            {title}
-          </summary>
-        </div>
-        {show && (
-          <div className="pl-6 pb-2 flex">
-            <section
-              className={classNames(
-                styles.Container,
-                styles.collapse_Container
-              )}
-            >
-              {children}
-            </section>
-          </div>
-        )}
+          >
+            expand_more
+          </i>
+        </summary>
+        {show && <div className={styles.PanelContent}>{children}</div>}
       </div>
     </section>
   );
