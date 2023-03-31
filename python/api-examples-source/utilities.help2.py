5de3bcb9b96228342b91ccbd909b317ff40f45c2
snehankekre <snehan.minerva@gmail.com>
2023-03-31 10:54:25 +0530

    Replace Dataframe with .read_csv in utilities.help2.py
    Former-commit-id: 6535a627d33b378c55c997b1ce2c1823468b69ef

--- python/api-examples-source/utilities.help2.py
+++ python/api-examples-source/utilities.help2.py
@@ -1,11 +1,8 @@
 import pandas
 import streamlit as st
 
-col1, col2 = st.columns(2)
-with col1:
-    # Get help for Pandas DataFrame:
-    pandas.DataFrame
+# Get help for Pandas read_csv:
+pandas.DataFrame
 
-with col2:
-    # Get help for Streamlit itself:
-    st
+# Get help for Streamlit itself:
+st
