320a5dd9965ac5d2785963d987990dbabbdcab11
Juan Martin Garcia <juan.garcia@snowflake.com>
2023-04-27 10:47:15 -0300

    Optimize images

--- components/blocks/componentSlider.js
+++ components/blocks/componentSlider.js
@@ -1,7 +1,7 @@
 import React, { useState, useRef } from "react";
-import Slider from "react-slick";
 import classNames from "classnames";
 
+import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 
