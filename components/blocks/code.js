1b5c9da9707f518c3394fc9d8a3319b36c5e8575
Juan Martin Garcia <juan.garcia@snowflake.com>
2023-05-12 16:22:13 -0300

    WIP: Try turning syntax highlighting off

--- components/blocks/code.js
+++ components/blocks/code.js
@@ -1,35 +1,35 @@
 import React, { useEffect } from "react";
 import classNames from "classnames";
-import Prism from "prismjs";
-import "prismjs/components/prism-bash";
-import "prismjs/components/prism-docker";
-import "prismjs/components/prism-jsx";
-import "prismjs/components/prism-python";
-import "prismjs/components/prism-sql";
-import "prismjs/components/prism-toml";
-import "prismjs/components/prism-yaml";
-import "prismjs/plugins/line-numbers/prism-line-numbers";
-import "prismjs/plugins/line-highlight/prism-line-highlight";
-import "prismjs/plugins/line-highlight/prism-line-highlight.css";
-import "prismjs/plugins/toolbar/prism-toolbar";
-import "prismjs/plugins/copy-to-clipboard/prism-copy-to-clipboard";
-import "prismjs/plugins/normalize-whitespace/prism-normalize-whitespace";
+// import Prism from "prismjs";
+// import "prismjs/components/prism-bash";
+// import "prismjs/components/prism-docker";
+// import "prismjs/components/prism-jsx";
+// import "prismjs/components/prism-python";
+// import "prismjs/components/prism-sql";
+// import "prismjs/components/prism-toml";
+// import "prismjs/components/prism-yaml";
+// import "prismjs/plugins/line-numbers/prism-line-numbers";
+// import "prismjs/plugins/line-highlight/prism-line-highlight";
+// import "prismjs/plugins/line-highlight/prism-line-highlight.css";
+// import "prismjs/plugins/toolbar/prism-toolbar";
+// import "prismjs/plugins/copy-to-clipboard/prism-copy-to-clipboard";
+// import "prismjs/plugins/normalize-whitespace/prism-normalize-whitespace";
 
 import Image from "./image";
 
 import styles from "./code.module.css";
 
 const Code = ({ code, children, language, img, lines }) => {
-  useEffect(() => {
-    if (window) {
-      window.initial = { prism: true };
-      Prism.highlightAll();
-    }
+  // useEffect(() => {
+  //   if (window) {
+  //     window.initial = { prism: true };
+  //     Prism.highlightAll();
+  //   }
 
-    return () => {
-      window.initial.prism = false;
-    };
-  }, []);
+  //   return () => {
+  //     window.initial.prism = false;
+  //   };
+  // }, []);
 
   let ConditionalRendering;
   let customCode = code !== undefined ? code : children;
