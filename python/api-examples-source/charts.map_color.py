5f8b4ccd8ee08befe0ab59e10ce1300ea7c44579
Debbie Matthews <debbie.matthews@snowflake.com>
2023-07-13 22:49:08 -0700

    Embedded app for st.map color and size support
    This commit adds the source code for another embedded app for st.map. The app demonstrates the newly added feature to declare size and color as well as latitude and longitude by any column name.

---
+++ python/api-examples-source/charts.map_color.py
@@ -0,0 +1,16 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+
+df = pd.DataFrame({
+    "col1": np.random.randn(1000) / 50 + 37.76,
+    "col2": np.random.randn(1000) / 50 + -122.4,
+    "col3": np.random.randn(1000) * 100,
+    "col4": np.random.rand(1000, 4).tolist(),
+})
+
+st.map(df,
+    latitude='col1',
+    longitude='col2',
+    size='col3',
+    color='col4')
