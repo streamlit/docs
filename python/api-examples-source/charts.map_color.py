b3f17e6c34724ec4efbababb2a4012e70dd7f5f3
snehankekre <snehan.minerva@gmail.com>
2023-07-14 12:20:26 +0530

    Cache dataframe to avoid memory leak

--- python/api-examples-source/charts.map_color.py
+++ python/api-examples-source/charts.map_color.py
@@ -1,16 +1,20 @@
-import streamlit as st
-import pandas as pd
 import numpy as np
+import pandas as pd
+import streamlit as st
 
-df = pd.DataFrame({
-    "col1": np.random.randn(1000) / 50 + 37.76,
-    "col2": np.random.randn(1000) / 50 + -122.4,
-    "col3": np.random.randn(1000) * 100,
-    "col4": np.random.rand(1000, 4).tolist(),
-})
 
-st.map(df,
-    latitude='col1',
-    longitude='col2',
-    size='col3',
-    color='col4')
+@st.cache_data
+def load_data():
+    return pd.DataFrame(
+        {
+            "col1": np.random.randn(1000) / 50 + 37.76,
+            "col2": np.random.randn(1000) / 50 + -122.4,
+            "col3": np.random.randn(1000) * 100,
+            "col4": np.random.rand(1000, 4).tolist(),
+        }
+    )
+
+
+df = load_data()
+
+st.map(df, latitude="col1", longitude="col2", size="col3", color="col4")
