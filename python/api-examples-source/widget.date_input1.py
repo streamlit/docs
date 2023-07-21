1febe8cc273d580fb10caad8758cf73875976a6b
snehankekre <snehan.minerva@gmail.com>
2023-07-21 12:08:20 +0200

    Display result

--- python/api-examples-source/widget.date_input1.py
+++ python/api-examples-source/widget.date_input1.py
@@ -7,10 +7,12 @@ next_year = today.year + 1
 jan_1 = datetime.date(next_year, 1, 1)
 dec_31 = datetime.date(next_year, 12, 31)
 
-st.date_input(
+d = st.date_input(
     "Select your vacation for next year",
     (jan_1, datetime.date(next_year, 1, 7)),
     jan_1,
     dec_31,
     format="MM.DD.YYYY",
 )
+
+d
