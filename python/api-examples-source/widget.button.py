187cc8e3bad215f95a677b155b45fb1da8a158a8
Debbie Matthews <debbie.matthews@snowflake.com>
2023-09-15 07:57:57 -0700

    Prerelease 1.27.0 (#805)
    * Update to streamlit-nightly for api examples
    
    * st.scatter embedded app source
    
    * Add st.progress example source code
    
    * Simple charts PEP 8 spacing
    
    * st.link_button example
    
    * Setting none as initial value examples
    
    * Format embedded apps with black
    
    * Line wrapping in text.text_area.py
    
    ---------
    
    Co-authored-by: snehankekre <snehan.minerva@gmail.com>

--- python/api-examples-source/widget.button.py
+++ python/api-examples-source/widget.button.py
@@ -1,7 +1,7 @@
 import streamlit as st
 
-st.button('Reset', type='primary')
-if st.button('Say hello'):
-    st.write('Why hello there')
+st.button("Reset", type="primary")
+if st.button("Say hello"):
+    st.write("Why hello there")
 else:
-    st.write('Goodbye')
+    st.write("Goodbye")
--- python/api-examples-source/widget.number_input_empty.py
+++ python/api-examples-source/widget.number_input_empty.py
@@ -1,4 +1,4 @@
 import streamlit as st
 
-number = st.number_input("Insert a number")
+number = st.number_input("Insert a number", value=None)
 st.write("The current number is ", number)
