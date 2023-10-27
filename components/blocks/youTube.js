f111843f54e53ca11a792e38784a17cae3f21b2d
snehankekre <snehan.minerva@gmail.com>
2023-10-27 11:15:47 +0530

    Refactor: conditionally render caption

--- components/blocks/youTube.js
+++ components/blocks/youTube.js
@@ -5,26 +5,8 @@ import styles from "./youtube.module.css";
 const YouTube = ({ caption, videoId, start, end }) => {
   let YouTubeBlock;
 
-  if (caption) {
-    YouTubeBlock = (
-      <section className={styles.Container}>
-        <section className={styles.IframeContainer}>
-          <iframe
-            src={`https://www.youtube-nocookie.com/embed/${videoId}?rel=0&start=${start}&end=${end}`}
-            className={styles.Iframe}
-            title="YouTube video player"
-            frameBorder="0"
-            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-            allowFullScreen
-          ></iframe>
-        </section>
-        <section className={styles.CaptionContainer}>
-          <p className={styles.Caption}>{caption}</p>
-        </section>
-      </section>
-    );
-  } else {
-    YouTubeBlock = (
+  YouTubeBlock = (
+    <section className={styles.Container}>
       <section className={styles.IframeContainer}>
         <iframe
           src={`https://www.youtube-nocookie.com/embed/${videoId}?rel=0&start=${start}&end=${end}`}
@@ -35,8 +17,13 @@ const YouTube = ({ caption, videoId, start, end }) => {
           allowFullScreen
         ></iframe>
       </section>
-    );
-  }
+      {caption && (
+        <section className={styles.CaptionContainer}>
+          <p className={styles.Caption}>{caption}</p>
+        </section>
+      )}
+    </section>
+  );
   return YouTubeBlock;
 };
 
