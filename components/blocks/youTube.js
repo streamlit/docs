510aa509b57961d1e14547405a4bc1578d3ded74
snehankekre <snehan.minerva@gmail.com>
2023-10-27 11:42:37 +0530

    Add autoplay and loop support to YouTube component

--- components/blocks/youTube.js
+++ components/blocks/youTube.js
@@ -2,14 +2,19 @@ import React from "react";
 
 import styles from "./youtube.module.css";
 
-const YouTube = ({ caption, videoId, start, end }) => {
+const YouTube = ({ caption, videoId, start, end, autoplay, loop }) => {
   let YouTubeBlock;
+  let mute = 0;
+
+  if (autoplay || loop) {
+    mute = 1;
+  }
 
   YouTubeBlock = (
     <section className={styles.Container}>
       <section className={styles.IframeContainer}>
         <iframe
-          src={`https://www.youtube-nocookie.com/embed/${videoId}?rel=0&start=${start}&end=${end}`}
+          src={`https://www.youtube-nocookie.com/embed/${videoId}?playlist=${videoId}&rel=0&start=${start}&end=${end}&autoplay=${autoplay}&loop=${loop}&mute=${mute}`}
           className={styles.Iframe}
           title="YouTube video player"
           frameBorder="0"
