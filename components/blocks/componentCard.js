872e9e3b9551bb6a97cc014f80729f1804430d48
snehankekre <snehan.minerva@gmail.com>
2024-01-10 19:18:11 +0530

    Refactor containsAnchor utility into separate script
    Former-commit-id: ae02ee19b7f0e654d1b974872ec8653fb0f6e5c6

--- components/blocks/componentCard.js
+++ components/blocks/componentCard.js
@@ -3,20 +3,7 @@ import React from "react";
 
 import genericStyles from "./refCard.module.css";
 import customStyles from "./componentCard.module.css";
-
-// Utility function to check if children contain an anchor tag
-const containsAnchor = (children) => {
-  let containsAnchorTag = false;
-  React.Children.forEach(children, (child) => {
-    if (child?.type === "a") {
-      containsAnchorTag = true;
-    } else if (child?.props?.children) {
-      containsAnchorTag =
-        containsAnchorTag || containsAnchor(child.props.children);
-    }
-  });
-  return containsAnchorTag;
-};
+import { containsAnchor } from "../../scripts/contains-anchor";
 
 const ComponentCard = ({ children, href }) => {
   const containsAnchorTag = containsAnchor(children);
