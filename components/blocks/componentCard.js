5aad8819cfad980132eb713ed3947fc191d75c01
snehankekre <snehan.minerva@gmail.com>
2024-01-10 19:04:37 +0530

    Refactor `RefCard` and `ComponentCard` to resolve React hydration errors
    Former-commit-id: 6fe1510df2ea6bb3d2394a1d53ee5d9f21ce236d

--- components/blocks/componentCard.js
+++ components/blocks/componentCard.js
@@ -1,19 +1,48 @@
 import classNames from "classnames";
+import React from "react";
 
 import genericStyles from "./refCard.module.css";
 import customStyles from "./componentCard.module.css";
 
+// Utility function to check if children contain an anchor tag
+const containsAnchor = (children) => {
+  let containsAnchorTag = false;
+  React.Children.forEach(children, (child) => {
+    if (child?.type === "a") {
+      containsAnchorTag = true;
+    } else if (child?.props?.children) {
+      containsAnchorTag =
+        containsAnchorTag || containsAnchor(child.props.children);
+    }
+  });
+  return containsAnchorTag;
+};
+
 const ComponentCard = ({ children, href }) => {
-  return (
-    <a
-      className={classNames(genericStyles.Container, customStyles.Container)}
-      href={href}
-      target="_blank"
-      rel="noopener noreferrer"
-    >
-      {children}
-    </a>
-  );
+  const containsAnchorTag = containsAnchor(children);
+
+  const handleCardClick = (e) => {
+    if (e.target.tagName !== "A") {
+      window.open(href, "_blank", "noopener,noreferrer");
+    }
+  };
+
+  const Wrapper = containsAnchorTag ? "div" : "a";
+  const wrapperProps = containsAnchorTag
+    ? {
+        onClick: handleCardClick,
+        className: classNames(genericStyles.Container, customStyles.Container),
+        role: "link",
+        tabIndex: "0",
+      }
+    : {
+        href,
+        target: "_blank",
+        rel: "noopener noreferrer",
+        className: classNames(genericStyles.Container, customStyles.Container),
+      };
+
+  return <Wrapper {...wrapperProps}>{children}</Wrapper>;
 };
 
 export default ComponentCard;
