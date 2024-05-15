71d4477e058f6a727f27fadd80ee515feb617fd4
Debbie Matthews <debbie.matthews@snowflake.com>
2024-05-15 11:52:13 -0700

    Hide Release notes link in header for xs screens

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,6 +11,7 @@ module.exports = {
     screens: {
       // Remove largest breakpoint, while keeping everything else at Tailwind defaults.
       // https://tailwindcss.com/docs/breakpoints
+      xs: "350px",
       sm: "640px",
       md: "768px",
       lg: "1024px",
