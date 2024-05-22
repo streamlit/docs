28a592d58b4416a91267723d8ff49aa2f7026de7
Debbie Matthews <debbie.matthews@snowflake.com>
2024-05-22 09:48:50 -0700

    Add path from root for logo example (#1075)
    Former-commit-id: 032b99271db351f1bb38b1662817096ff21ece71

--- python/api-examples-source/media.logo.py
+++ python/api-examples-source/media.logo.py
@@ -9,5 +9,8 @@ options = [HORIZONTAL_RED, ICON_RED, HORIZONTAL_BLUE, ICON_BLUE]
 sidebar_logo = st.selectbox("Sidebar logo", options, 0)
 main_body_logo = st.selectbox("Main body logo", options, 1)
 
-st.logo(sidebar_logo, icon_image=main_body_logo)
+st.logo(
+    f"python/api-examples-source/{sidebar_logo}",
+    icon_image=f"python/api-examples-source/{main_body_logo}",
+)
 st.sidebar.markdown("Hi!")
