a0d3593773d76ff68885f6d98a1bbabc5b2105a0
Debbie Matthews <debbie.matthews@snowflake.com>
2024-05-22 13:31:05 -0700

    Add selection highlight to Vega-Altair example

--- python/api-examples-source/charts.altair_selections.py
+++ python/api-examples-source/charts.altair_selections.py
@@ -11,14 +11,20 @@ def load_data():
 
 df = load_data()
 
+point_selector = alt.selection_point("point_selection")
+interval_selector = alt.selection_interval("interval_selection")
 chart = (
     alt.Chart(df)
     .mark_circle()
-    .encode(x="a", y="b", size="c", color="c", tooltip=["a", "b", "c"])
-    .add_params(
-        alt.selection_interval("interval_selection"),
-        alt.selection_point("point_selection"),
+    .encode(
+        x="a",
+        y="b",
+        size="c",
+        color="c",
+        tooltip=["a", "b", "c"],
+        fillOpacity=alt.condition(point_selector, alt.value(1), alt.value(0.3)),
     )
+    .add_params(point_selector, interval_selector)
 )
 
 st.altair_chart(chart, key="alt_chart", on_select="rerun")
