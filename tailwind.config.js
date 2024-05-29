4a266cfff09c305c31819e6341e8330b3dde3853
Debbie Matthews <debbie.matthews@snowflake.com>
2024-05-29 14:14:11 -0700

    Revert "Hide Release notes link in header for xs screens"
    This reverts commit 71d4477e058f6a727f27fadd80ee515feb617fd4.

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,7 +11,6 @@ module.exports = {
     screens: {
       // Remove largest breakpoint, while keeping everything else at Tailwind defaults.
       // https://tailwindcss.com/docs/breakpoints
-      xs: "350px",
       sm: "640px",
       md: "768px",
       lg: "1024px",
