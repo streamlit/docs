f3315749d9396a04d06e0d209127fb5f6395732a
Debbie Matthews <debbie.matthews@snowflake.com>
2024-07-29 14:57:15 -0700

    Move chat button to bottom-right corner

--- components/navigation/header.js
+++ components/navigation/header.js
@@ -2,7 +2,6 @@ import { debounce } from "lodash";
 import React, { useState, useEffect } from "react";
 import classNames from "classnames";
 import Link from "next/link";
-import Kapa from "../utilities/kapa";
 import dynamic from "next/dynamic";
 
 // import Navigation from
@@ -52,7 +51,6 @@ const Header = ({ isSticky }) => {
         </Link>
         <section className={styles.NavigationContainer}>
           <Search />
-          <Kapa />
           <ThemeToggle />
           {mobileNav}
         </section>
--- components/utilities/kapa.js
+++ components/utilities/kapa.js
@@ -1,4 +1,3 @@
-import classNames from "classnames";
 import styles from "./kapa.module.css";
 
 const Kapa = () => {
@@ -10,47 +9,49 @@ const Kapa = () => {
     document.getElementsByClassName(styles.Tooltip)[0].style.display = "none";
   };
   let kapaWidget = (
-    <section className={styles.AskButtonContainer}>
-      <button
-        id="kapa-ai"
-        type="button"
-        className={styles.AskButton}
-        onMouseOver={showTooltip}
-        onMouseOut={hideTooltip}
-      >
-        <i className={styles.AskIcon}>forum</i>
-      </button>
-      <div className={styles.Tooltip}>
-        <p>Try our new docs assistant!</p>
-      </div>
-      <script
-        src="https://widget.kapa.ai/kapa-widget.bundle.js"
-        data-website-id="e81c2b35-6c03-4576-a56c-3c825f866e06"
-        data-project-name="Streamlit"
-        data-project-color="#000000"
-        data-project-logo="https://docs.streamlit.io/logo.svg"
-        data-modal-title="Streamlit Docs assistant"
-        data-modal-disclaimer="
-              This AI chatbot is powered by kapa.ai and public Streamlit information.
-              Answers may be inaccurate, inefficient, or biased. Any use or decisions
-              based on such answers should include reasonable practices including
-              human oversight to ensure they are safe, accurate, and suitable for
-              your intended purpose. Streamlit is not liable for any actions, losses,
-              or damages resulting from the use of the chatbot. 
+    <div className={styles.FooterContainer}>
+      <section className={styles.AskButtonContainer}>
+        <button
+          id="kapa-ai"
+          type="button"
+          className={styles.AskButton}
+          onMouseOver={showTooltip}
+          onMouseOut={hideTooltip}
+        >
+          <i className={styles.AskIcon}>forum</i> Ask AI
+        </button>
+        <div className={styles.Tooltip}>
+          <p>Try our new docs assistant!</p>
+        </div>
+        <script
+          src="https://widget.kapa.ai/kapa-widget.bundle.js"
+          data-website-id="e81c2b35-6c03-4576-a56c-3c825f866e06"
+          data-project-name="Streamlit"
+          data-project-color="#000000"
+          data-project-logo="https://docs.streamlit.io/logo.svg"
+          data-modal-title="Streamlit Docs assistant"
+          data-modal-disclaimer="
+                This AI chatbot is powered by kapa.ai and public Streamlit information.
+                Answers may be inaccurate, inefficient, or biased. Any use or decisions
+                based on such answers should include reasonable practices including
+                human oversight to ensure they are safe, accurate, and suitable for
+                your intended purpose. Streamlit is not liable for any actions, losses,
+                or damages resulting from the use of the chatbot. 
 
-              Do not enter any private, sensitive, personal, or regulated data.
-              By using this chatbot, you acknowledge and agree that input you provide
-              and answers you receive (collectively, “Content”) may be used by
-              Streamlit and kapa.ai to provide, maintain, develop, and improve
-              their respective offerings. For more information on how
-              kapa.ai may use your Content, see https://www.kapa.ai/content/terms-of-service."
-        data-button-hide="true"
-        data-modal-override-open-id="kapa-ai"
-        data-modal-lock-scroll="false"
-        data-modal-border-radius="6px"
-        data-modal-image-height="18px"
-      ></script>
-    </section>
+                Do not enter any private, sensitive, personal, or regulated data.
+                By using this chatbot, you acknowledge and agree that input you provide
+                and answers you receive (collectively, “Content”) may be used by
+                Streamlit and kapa.ai to provide, maintain, develop, and improve
+                their respective offerings. For more information on how
+                kapa.ai may use your Content, see https://www.kapa.ai/content/terms-of-service."
+          data-button-hide="true"
+          data-modal-override-open-id="kapa-ai"
+          data-modal-lock-scroll="false"
+          data-modal-border-radius="6px"
+          data-modal-image-height="18px"
+        ></script>
+      </section>
+    </div>
   );
 
   return kapaWidget;
