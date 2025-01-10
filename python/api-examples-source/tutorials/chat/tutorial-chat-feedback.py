8289844fadd87d2dea2b55af9bd8972ec0b7bac2
Debbie Matthews <debbie.matthews@snowflake.com>
2025-01-10 10:51:15 -0800

    Embedded apps for chat tutorials (#1218)
    * Add chat tutorial app sources
    
    * Update tutorial-chat-feedback.py
    
    * Update chat app sources
    
    Former-commit-id: 85cc4e6b4276cd1c18781d2209633d037e6c9b19

---
+++ python/api-examples-source/tutorials/chat/tutorial-chat-feedback.py
@@ -0,0 +1,45 @@
+import streamlit as st
+import time
+
+
+def chat_stream(prompt):
+    response = f'You said, "{prompt}" ...interesting.'
+    for char in response:
+        yield char
+        time.sleep(0.02)
+
+
+def save_feedback(index):
+    st.session_state.history[index]["feedback"] = st.session_state[f"feedback_{index}"]
+
+
+if "history" not in st.session_state:
+    st.session_state.history = []
+
+for i, message in enumerate(st.session_state.history):
+    with st.chat_message(message["role"]):
+        st.write(message["content"])
+        if message["role"] == "assistant":
+            feedback = message.get("feedback", None)
+            st.session_state[f"feedback_{i}"] = feedback
+            st.feedback(
+                "thumbs",
+                key=f"feedback_{i}",
+                disabled=feedback is not None,
+                on_change=save_feedback,
+                args=[i],
+            )
+
+if prompt := st.chat_input("Say something"):
+    with st.chat_message("user"):
+        st.write(prompt)
+    st.session_state.history.append({"role": "user", "content": prompt})
+    with st.chat_message("assistant"):
+        response = st.write_stream(chat_stream(prompt))
+        st.feedback(
+            "thumbs",
+            key=f"feedback_{len(st.session_state.history)}",
+            on_change=save_feedback,
+            args=[len(st.session_state.history)],
+        )
+    st.session_state.history.append({"role": "assistant", "content": response})
