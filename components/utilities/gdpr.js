071b2315de7794fc1efa1a285a9e179fda26f85c
Thiago Teixeira <thiago.teixeira@snowflake.com>
2025-03-11 15:36:04 -0700

    Fix z-indices all around

--- components/utilities/gdpr.js
+++ components/utilities/gdpr.js
@@ -84,7 +84,7 @@ export default function GDPRBanner({
         <div
           className={classNames(
             isTelemetryBannerVisible === false ? "hidden" : "",
-            "z-50 fixed",
+            "z-30 fixed",
             "bottom-2 inset-x-2 md:bottom-4 md:inset-x-4",
           )}
         >
