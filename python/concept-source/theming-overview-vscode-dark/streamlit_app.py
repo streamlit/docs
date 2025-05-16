2ebdcbe67cc0488f9a3e5cecb472ff89ca53286a
Debbie Matthews <debbie.matthews@snowflake.com>
2025-05-15 19:39:45 -0700

    Fix VS code dark theme example

--- python/concept-source/theming-overview-vscode-dark/streamlit_app.py
+++ python/concept-source/theming-overview-vscode-dark/streamlit_app.py
@@ -1,2 +1,4 @@
 from streamlit.hello import streamlit_app
 streamlit_app.run()
+
+st.sidebar.caption("This app uses [Space Grotesk](https://fonts.google.com/specimen/Space+Grotesk) and [Space Mono](https://fonts.google.com/specimen/Space+Mono) fonts.")
