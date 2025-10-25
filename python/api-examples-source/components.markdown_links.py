bb3b4f064fb804510c3e38c565694478d16af322
Debbie Matthews <debbie.matthews@snowflake.com>
2025-10-24 17:09:15 -0700

    Example apps for custom components v2

---
+++ python/api-examples-source/components.markdown_links.py
@@ -0,0 +1,33 @@
+import streamlit as st
+
+JS = """
+export default function(component) {
+    const { setTriggerValue } = component;
+    const links = document.querySelectorAll('a[href="#"]');
+
+    links.forEach((link) => {
+        link.onclick = (e) => {
+            setTriggerValue('clicked', link.innerHTML);
+        };
+    });
+}
+"""
+
+my_component = st.components.v2.component(
+    "inline_links",
+    js=JS,
+)
+
+
+def callback():
+    pass
+
+
+result = my_component(on_clicked_change=callback)
+
+st.markdown(
+    "Components aren't [sandboxed](#), so you can write JS that [interacts](#) with the main [document](#)."
+)
+
+if result.clicked:
+    st.write(f"You clicked {result.clicked}!")
