b7055b1ce44f7469dca82f1e3a76be194177d304
Debbie Matthews <debbie.matthews@snowflake.com>
2025-12-02 22:18:07 -0800

    Page link query params example

--- python/api-examples-source/utilities.page_link_query_params/your_app.py
+++ python/api-examples-source/utilities.page_link_query_params/your_app.py
@@ -3,8 +3,7 @@ import streamlit as st
 
 def page_1():
     st.title("Page 1")
-    if st.button("Switch to Page 2"):
-        st.switch_page("page_2.py", query_params={"utm_source": "page_1"})
+    st.page_link("page_2.py", query_params={"utm_source": "page_1"})
 
 
 pg = st.navigation([page_1, "page_2.py"])
