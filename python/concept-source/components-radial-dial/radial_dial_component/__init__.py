d29fdfe4212064407a71a70d82df5d1fa9dd0e86
Debbie Matthews <debbie.matthews@snowflake.com>
2025-12-08 16:05:16 -0800

    Radial dial example

---
+++ python/concept-source/components-radial-dial/radial_dial_component/__init__.py
@@ -0,0 +1,25 @@
+from pathlib import Path
+import streamlit as st
+
+component_dir = Path(__file__).parent
+
+
+@st.cache_data
+def load_component_code():
+    with open(component_dir / "dial.css", "r") as f:
+        CSS = f.read()
+    with open(component_dir / "dial.html", "r") as f:
+        HTML = f.read()
+    with open(component_dir / "dial.js", "r") as f:
+        JS = f.read()
+    return HTML, CSS, JS
+
+
+HTML, CSS, JS = load_component_code()
+
+radial_dial = st.components.v2.component(
+    name="radial_dial",
+    html=HTML,
+    css=CSS,
+    js=JS,
+)
