2a81f8b5b5e5c03ac6231837aa09962b36ca5208
Debbie Matthews <debbie.matthews@snowflake.com>
2025-12-17 08:49:09 -0800

    Rename files

--- python/concept-source/components-interactive-counter/my_component/__init__.py
+++ python/concept-source/components-interactive-counter/my_component/__init__.py
@@ -6,17 +6,17 @@ _COMPONENT_DIR = Path(__file__).parent
 
 @st.cache_data
 def load_html():
-    with open(_COMPONENT_DIR / "my_html.html", "r") as f:
+    with open(_COMPONENT_DIR / "component.html", "r") as f:
         return f.read()
 
 @st.cache_data
 def load_css():
-    with open(_COMPONENT_DIR / "my_css.css", "r") as f:
+    with open(_COMPONENT_DIR / "component.css", "r") as f:
         return f.read()
 
 @st.cache_data
 def load_js():
-    with open(_COMPONENT_DIR / "my_js.js", "r") as f:
+    with open(_COMPONENT_DIR / "component.js", "r") as f:
         return f.read()
 
 HTML = load_html()
